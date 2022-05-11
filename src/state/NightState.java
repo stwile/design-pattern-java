b1686aea62067440ea2f049dd8ff31ebe167db3c
hyroky867 <handm755@gmail.com>
2022-05-12 00:22:26 +0900

    19. State Pattern

---
+++ src/state/NightState.java
@@ -0,0 +1,38 @@
+package state;
+
+public class NightState implements State {
+  private static final NightState singleton = new NightState();
+
+  private NightState() {}
+
+  public static NightState getInstance() {
+    return singleton;
+  }
+
+  @Override
+  public void doClock(Context context, int hour) {
+    if (9 <= hour && hour < 17) {
+      context.changeState(DayState.getInstance());
+    }
+  }
+
+  @Override
+  public void doUse(Context context) {
+    context.callSecurityCenter("非常：夜間の金庫使用！");
+  }
+
+  @Override
+  public void doAlarm(Context context) {
+    context.callSecurityCenter("非常ベル（夜間）");
+  }
+
+  @Override
+  public void doPhone(Context context) {
+    context.recordLog("夜間の通話記録");
+  }
+
+  @Override
+  public String toString() {
+    return "[夜間]";
+  }
+}
